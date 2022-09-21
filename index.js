0113f39da359f79555db53d8a01d373c86381cfc
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-09-21 15:41:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ function run() {
     partyparrot: {
       name: 'parrot',
       url: 'static/emoji/1f99c.png'
+    },
+    alert: {
+      name: 'alarm_clock',
+      url: 'static/emoji/23f0.png'
     }
   }
 
   for( let em in emojiMap ) {
-    document.querySelectorAll('button[id$="-partyparrot"] span.Reaction__emoji.emoticon').forEach(n => {
-      n.style = 'color: white;'
+    document.querySelectorAll('span[data-emoticon=partyparrot] > span').forEach(n => {
       n.style = `background-image: url("${document.location.origin}/${emojiMap[em].url}")`;
-      n.innerHTML = emojiMap[em].name;
+    });
+    document.querySelectorAll('button[id$="-partyparrot"] span.Reaction__emoji.emoticon').forEach(n => {
+      n.style = `background-image: url("${document.location.origin}/${emojiMap[em].url}")`;
     });
   }
 }
